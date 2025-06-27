0ad9e3acd4e96d9a23a41bfb78cdf151be969097
HuynhNgocKhanh <huynhngockhanh1201@gmail.com>
2025-06-27 10:54:25 +0700

    Email service

---
+++ FandomHub.Application/Intefaces/Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FandomHub.Application.Intefaces.Services
+{
+	public interface IEmailService
+	{
+	}
+}
